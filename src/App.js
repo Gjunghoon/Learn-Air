8ea89ec32fbb2d8ac1f331491531f9713c99b8a3
gjunghoon <jhkwon7@korea.ac.kr>
2021-01-07 22:06:05 +0900

    기타: 여자친구 달래주기용

--- src/App.js
+++ src/App.js
@@ -1,51 +1,13 @@
 import React, { useEffect } from "react";
 import styled from "styled-components";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Home from "./containers/Home";
-import Login from "./containers/Login";
-import Class from "./containers/Class";
-import Category from "./containers/Category";
-import Search from "./containers/Search";
-import Mypage from "./containers/Mypage";
-import Review from "./containers/Review";
-import Class2 from "./containers/Class2";
 
 const App = () => {
-  useEffect(() => {
-    window.Kakao.init("f656e406e85c1da4f68c86ee67f02280");
-    // SDK 초기화 여부를 판단합니다.
-    console.log(window.Kakao.isInitialized());
-  }, []);
   return (
     <Container>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/class">
-            <Class />
-          </Route>
-          <Route exact path="/class2">
-            <Class2 />
-          </Route>
-          <Route exact path="/category">
-            <Category />
-          </Route>
-          <Route exact path="/search">
-            <Search />
-          </Route>
-          <Route exact path="/mypage">
-            <Mypage />
-          </Route>
-          <Route exact path="/review">
-            <Review />
-          </Route>
-        </Switch>
-      </Router>
+      <h2>지윤아 사당행</h2>
+      <img src="https://gjunghoon-tinder-clone.s3.ap-northeast-2.amazonaws.com/love.jpg" />
+      <p>If nothing lasts forever,</p>
+      <p>will you be my nothing?</p>
     </Container>
   );
 };
@@ -55,5 +17,9 @@ export default App;
 const Container = styled.div`
   margin: 0 auto;
   max-width: 375px;
+  > h2,
+  p {
+    text-align: center;
+  }
 `;
 const Body = styled.div``;
